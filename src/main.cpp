3801a235899fa4ad50deda44c21fe84287515f4d
Lemonochrme <robin.marn.pro@gmail.com>
2024-12-02 15:26:38 +0100

    SSD1306 Oled Screen "Hello World"

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+#define SSD1306_ADDR 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Hello, World!");
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SSD1306_ADDR)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Hello, world!");
+  display.display(); 
 }
 
 void loop() {
-  Serial.println("Hi !");
+  
 }
