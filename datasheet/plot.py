1b3d40e7d14f6a27b2a08104be838f44e5c7d7f1
Yohan Boujon <yo.boujon@gmail.com>
2025-01-16 17:13:29 +0100

    Added Simulation waveform.

--- datasheet/plot.py
+++ datasheet/plot.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 def convert_csv_to_table(input_file, multiplier=1):
@@ -7,11 +8,11 @@ def convert_csv_to_table(input_file, multiplier=1):
     float_values = [float(value) * multiplier for value in values]
     df = pd.DataFrame({'Values': float_values})
     
-    return df
+    return df['Values']
 
 def plot_data(x, x_label, y, y_label, plot_size=(10, 10)):
     plt.figure(figsize=plot_size)
-    plt.plot(x['Values'], y['Values'], linestyle='-', linewidth=2)
+    plt.plot(x, y, linestyle='-', linewidth=2)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.grid(True)
@@ -29,3 +30,7 @@ if __name__ == "__main__":
     voltage = convert_csv_to_table("peigne_voltage.csv")
     current = convert_csv_to_table("peigne_current.csv", 1e9)
     plot_data(voltage, "Voltage (V)", current, "Current ($nA$)", (10,5))
+    
+    voltage = convert_csv_to_table("simulation_vout.csv");
+    time = np.arange(0, len(voltage) * 2e-9, 2e-9)
+    plot_data(time, "Time (s)", voltage, "Voltage (V)", (5,5))
